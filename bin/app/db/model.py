394740f5c26fc5d813651f986f2d8c0def2bdf41
Chris Keele <christhekeele@gmail.com>
2020-11-15 23:31:06 -0800

    Remove accidentally recursively copied project from bin folder.

--- bin/app/db/model.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.ext.declarative import declarative_base
-
-
-class Base(object):
-    def to_dict(self):
-        return {column.name: getattr(self, column.name, None) for column in self.__table__.columns}
-
-
-Base = declarative_base(cls=Base)
--- bin/app/telemetry.py
+++
@@ -1,7 +0,0 @@
-import newrelic.agent
-
-EVENT_NAMESPACE = "DiscrepancyMonitoring"
-
-
-def report(event, data):
-    newrelic.agent.record_custom_event(EVENT_NAMESPACE, dict(**data, event=event))
